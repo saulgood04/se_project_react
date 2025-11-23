c700053a88d06f4ea6272289c2bf6db870c678bd
Saul Good <saulgood04@gmail.com>
2025-11-23 18:37:05 -0500

    ModalWithForm open & close

--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -1,16 +1,28 @@
 import "./ModalWithForm.css";
 
-function ModalWithForm({ children, buttonText, title }) {
+function ModalWithForm({
+  children,
+  buttonText,
+  title,
+  activeModal,
+  handleCloseClick,
+}) {
   return (
-    <div className="modal">
+    <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
-        <button type="button" className="modal__close">
+        <button
+          onClick={handleCloseClick}
+          type="button"
+          className="modal__close"
+        >
           CLOSE
         </button>
         <form className="modal__form">
           {children}
-          <button type="submit" className="modal__submit">{buttonText}</button>
+          <button type="submit" className="modal__submit">
+            {buttonText}
+          </button>
         </form>
       </div>
     </div>
