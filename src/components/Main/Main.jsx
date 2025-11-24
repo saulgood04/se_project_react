24d0f9e90eb5c45eca5c9002f1b6555fa42dbbde
Saul Good <saulgood04@gmail.com>
2025-11-23 20:16:22 -0500

    ItemModal setup

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -3,7 +3,7 @@ import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 import { defaultClothingItems } from "../../utils/constants";
 
-function Main({ weatherData }) {
+function Main({ weatherData, handleCardClick }) {
   return (
     <main>
       <WeatherCard />
@@ -13,12 +13,18 @@ function Main({ weatherData }) {
         </p>
         <ul className="cards__list">
           {defaultClothingItems
-          //.filter((item) => {
-           // return item.weather ===weatherData.type;
-          //})
-          .map((item) => {
-            return <ItemCard key={item._id} item={item}/>;
-          })}
+            //.filter((item) => {
+            // return item.weather ===weatherData.type;
+            //})
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={handleCardClick}
+                />
+              );
+            })}
         </ul>
       </section>
     </main>
--- src/components/ModalWithForm/ModalWithForm.jsx
+++ src/components/ModalWithForm/ModalWithForm.jsx
@@ -5,14 +5,14 @@ function ModalWithForm({
   buttonText,
   title,
   activeModal,
-  handleCloseClick,
+  onClose,
 }) {
   return (
     <div className={`modal ${activeModal === "add-garment" && "modal_opened"}`}>
       <div className="modal__content">
         <h2 className="modal__title">{title}</h2>
         <button
-          onClick={handleCloseClick}
+          onClick={onClose}
           type="button"
           className="modal__close"
         >
