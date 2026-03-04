1b0f51ecd302fdf818db2d42dfc246adf049fc2e
Saul Good <saulgood04@gmail.com>
2026-03-04 17:49:34 -0500

    fixed header spacing

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -18,52 +18,56 @@ function Header({
   const currentUser = useContext(CurrentUserContext);
   return (
     <header className="header">
-      <NavLink to="/" className="header__logo">
-        <img src={logo} alt="WTWR logo" />
-      </NavLink>
-      <p className="header__date-and-location">
-        {currentDate}, {weatherData?.city}
-      </p>
-      <div className="header__controls">
-        <ToggleSwitch />
-        {currentUser && (
-          <button
-            onClick={handleAddClick}
-            type="button"
-            className="header__add-clothes-btn"
-          >
-            + Add Clothes
-          </button>
-        )}
-      </div>
-      {currentUser ? (
-        <NavLink className="header__nav-link" to="/profile">
-          <div className="header__user-container">
-            <p className="header__username">{currentUser.name}</p>
-            {currentUser.avatar ? (
-              <img
-                src={currentUser.avatar}
-                alt={currentUser.name}
-                className="header__avatar"
-              />
-            ) : (
-              <div className="header__avatar-placeholder">
-                {currentUser.name[0].toUpperCase()}
-              </div>
-            )}
-          </div>
-        </NavLink>
-      ) : (
-        <div className="header__auth-buttons">
-          <button onClick={onRegisterClick} className="header__register-btn">
-            Sign Up
-          </button>
-          <button onClick={onLoginClick} className="header__login-btn">
-            Log In
-          </button>
-        </div>
+  <div className="header__left">
+    <NavLink to="/" className="header__logo">
+      <img src={logo} alt="WTWR logo" />
+    </NavLink>
+    <p className="header__date-and-location">
+      {currentDate}, {weatherData?.city}
+    </p>
+  </div>
+  <div className="header__right">
+    <div className="header__controls">
+      <ToggleSwitch />
+      {currentUser && (
+        <button
+          onClick={handleAddClick}
+          type="button"
+          className="header__add-clothes-btn"
+        >
+          + Add Clothes
+        </button>
       )}
-    </header>
+    </div>
+    {currentUser ? (
+      <NavLink className="header__nav-link" to="/profile">
+        <div className="header__user-container">
+          <p className="header__username">{currentUser.name}</p>
+          {currentUser.avatar ? (
+            <img
+              src={currentUser.avatar}
+              alt={currentUser.name}
+              className="header__avatar"
+            />
+          ) : (
+            <div className="header__avatar-placeholder">
+              {currentUser.name[0].toUpperCase()}
+            </div>
+          )}
+        </div>
+      </NavLink>
+    ) : (
+      <div className="header__auth-buttons">
+        <button onClick={onRegisterClick} className="header__register-btn">
+          Sign Up
+        </button>
+        <button onClick={onLoginClick} className="header__login-btn">
+          Log In
+        </button>
+      </div>
+    )}
+  </div>
+</header>
   );
 }
 
