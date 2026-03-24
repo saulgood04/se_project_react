f82a13527aed34dfb7124bd493617a2b08360086
Saul Good <saulgood04@gmail.com>
2026-03-24 14:32:01 -0400

    fixed delete button

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -284,7 +284,7 @@ function App() {
             isOpen={activeModal === "preview"}
             card={selectedCard}
             onClose={closeActiveModal}
-            onDeleteItem={onDeleteItem}
+            onDeleteItem={() => onDeleteItem(selectedCard._id)}
             onCardLike={handleCardLike}
           />
         </div>
